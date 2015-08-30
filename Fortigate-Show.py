9c229ccc5179873544c325e633b34428293e351a
Manny Fernandez <manny@mannygfernandez.com>
2015-08-30 05:32:26 -0400

    Added Fortinet

---
+++ Fortigate-Show.py
@@ -0,0 +1,16 @@
+﻿# $language = "python"
+# $interface = "1.0"
+
+# show running configuration on Fortigate
+
+def Main():
+	crt.Screen.Synchronous = True
+	crt.Screen.Send("config" + chr(9) + "system console" + chr(13))
+	crt.Screen.WaitForString(" (console) # ")
+	crt.Screen.Send("set output standard" + chr(13))
+	crt.Screen.WaitForString(" (console) # ")
+	crt.Screen.Send("n" + chr(8) + "end" + chr(13))
+	crt.Screen.WaitForString(" # ")
+	crt.Screen.Send("show full" + chr(9) + chr(13))
+
+Main()
