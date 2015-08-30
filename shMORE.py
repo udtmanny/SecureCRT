fdc23b516db74fcc3f2c970eaf9297420c9e28f4
Manny Fernandez <manny@mannygfernandez.com>
2015-08-29 23:47:23 -0400

    Uploaded shMore

---
+++ shMORE.py
@@ -0,0 +1,14 @@
+﻿# $language = "python"
+# $interface = "1.0"
+
+# Changes the pagination and does a 'more system:running'
+
+def Main():
+	crt.Screen.Synchronous = True
+	crt.Screen.Send("terminal pager 0" + chr(13))
+	crt.Screen.WaitForString(chr(13) + "#")
+	crt.Screen.Send("more system:running-config" + chr(13))
+	crt.Screen.WaitForString(chr(13) + "#")	
+	crt.Screen.Send("terminal pager 24" + chr(13))
+
+Main()
